209a9c8bbde6b05ac8e663eae249f77c0bedc5ec
XBXyftx <145579105+XBXyftx@users.noreply.github.com>
2024-03-31 14:27:28 +0800

    Delete 第四章数组 directory

--- 第四章数组/数组不为零的数.ts
+++
@@ -1,8 +0,0 @@
-let shuzu:number[] = [1,0,0,5,0,0]
-let newarray:number[] = []
-for (let i = 0; i < shuzu.length; i++){
-    if(shuzu[i]!==0){
-        newarray.push(shuzu[i])
-    }
-}
-console.log(newarray)
--- 第四章数组/数组最大值.ts
+++
@@ -1,2 +0,0 @@
-let op: number[]=[1,21,32,1,2,3,4,12,3333,21]
-console.log(Math.max(...op))
